e1c299200701290bb3c7121710ab6d1cf9280e82
vertefra <verte.fra@gmail.com>
2020-09-17 08:32:46 -0400

    comment section added

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function CommentCreate({ postId }) {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(`http://127.0.0.1:4001/posts/${postId}/comments`, {
+      content,
+    });
+    setContent("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">submit</button>
+      </form>
+    </div>
+  );
+}
