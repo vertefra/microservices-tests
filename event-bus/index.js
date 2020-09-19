ab967109ff85ca832edb911eea8fd99f43aae7d9
vertefra <verte.fra@gmail.com>
2020-09-19 11:15:16 -0400

    fix

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,10 +5,15 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
   console.log("received event: ", event);
+  console.log("storing event");
+
+  events.push(event);
 
   //  EVENTS DISPATCHER
 
@@ -18,6 +23,13 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
+});
+
+// event retriever
+// from this endpoint you can access all the events ever stored
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
